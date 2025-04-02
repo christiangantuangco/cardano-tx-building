9158952759b2d1ce131c4ec807acf5589efaec44
Christian Benedict Gantuangco <gantuangcoc98@gmail.com>
2025-04-02 11:50:35 +0800

    feat(dotnet): levvy lend tx builder template

--- dotnet/src/LevvyTxBuilding/Program.cs
+++ dotnet/src/LevvyTxBuilding/Program.cs
@@ -1,7 +1,15 @@
-﻿using Chrysalis.Cbor.Serialization;
-using Chrysalis.Cbor.Types;
+﻿using Chrysalis.Cbor.Types;
 using Chrysalis.Cbor.Types.Plutus.Address;
+using Chrysalis.Tx.Builders;
+using Chrysalis.Tx.Providers;
+using Address = Chrysalis.Wallet.Models.Addresses.Address;
 using LevvyTxBuilding.Data.Types;
+using Chrysalis.Cbor.Types.Cardano.Core.Common;
+using Chrysalis.Cbor.Serialization;
+
+var provider = new Blockfrost("previewajMhMPYerz9Pd3GsqjayLwP5mgnNnZCC");
+Address tanAddress = Address.FromBech32("addr_test1qzxhwhx5ayuhcg6e7xcufy0ta6z5z2q6hwjl8m2r9cmcst0n3lfh3d6a7ege7gepfnhz2gxnm2rsvyd7yngf878k47wqjrmaqk");
+Address validatorAddress = Address.FromBech32("addr_test1wqmglnv5j97jtl3ley6upj4g9q2cn5wc7ra9numaepa6taqch4ya9");
 
 LendDetails lendDetails = new(
     Lender: new(
@@ -18,6 +26,19 @@ LendDetails lendDetails = new(
     LoanDuration: 76900000,
     LevvyType: new Tokens()
 );
-
 byte[] lendDetailsBytes = CborSerializer.Serialize(lendDetails);
-Console.WriteLine(Convert.ToHexString(lendDetailsBytes));
+
+var transfer = TransactionTemplateBuilder<ulong>.Create(provider)
+.AddStaticParty("tan", tanAddress.ToBech32(), true)
+.AddStaticParty("validator", validatorAddress.ToBech32())
+.AddInput((options, amount) =>
+{
+    options.From = "tan";
+})
+.AddOutput((options, amount) =>
+{
+    options.To = "validator";
+    options.Amount = new Lovelace(amount); // @TODO calculate fee helper function
+    options.Datum = new InlineDatumOption(1, new(lendDetailsBytes));
+})
+.Build();
